17948016d8d10df7c40c20a662a18cecc36560c8
Samily Leal <lealsamilysuga@gmail.com>
2023-08-29 14:25:29 -0300

    fixed

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -9,12 +9,28 @@ export class ImcComponent {
   peso:number = 0;
   altura:number = 0;
   resultado:number = 0;
+  classificacao:string = '';
 
   calcular(){
     if(this.peso > 0 && this.altura > 0){
       const alturaM = this.altura / 100;
 
       this.resultado = this.peso / (alturaM * alturaM);
+
+      if(this.resultado < 18.5){
+        this.classificacao = 'Magreza';
+      } else if(this.resultado < 24.9){
+        this.classificacao = 'Peso normal';
+      } else if(this.resultado < 29.9){
+        this.classificacao = 'Sobrepeso';
+      } else if(this.resultado < 34.9){
+        this.classificacao = 'Obesidade I';
+      } else if(this.resultado < 40){
+        this.classificacao = 'Obesidade II';
+      } else {
+        this.classificacao = 'Obesidade III';
+      }
+      
     } else {
       this.resultado = 0;
     }
